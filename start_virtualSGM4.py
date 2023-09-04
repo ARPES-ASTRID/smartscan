0fbae49aaeb5e7432b88e5f874206d2493be165a
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2023-09-04 11:26:51 +0200

    setup logger for virtualSGM4

--- start_virtualSGM4.py
+++ start_virtualSGM4.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from pathlib import Path
 import asyncio
+import logging
 
 if __name__ == '__main__':
 
@@ -11,6 +12,22 @@ if __name__ == '__main__':
     source_file =  Path(r"D:\data\SGM4\SmartScan\Z006_35_0.h5")
 
     name = Path(source_file).stem
+
+        # init logger
+    logger = logging.getLogger('virtualSGM4')
+    logger.setLevel('DEBUG')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s | %(message)s')
+    sh = logging.StreamHandler()
+    sh.setLevel('DEBUG')
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    # fh = logging.FileHandler(os.path.join(args.logdir, args.logfile))
+    # fh.setLevel(args.loglevel)
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+    logger.info('init VirtualSGM4 object')
+    # init scan manager
 
     vm = VirtualSGM4(
         'localhost', 

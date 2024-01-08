ba53b921291d0528f114c3c581679f4f94bfc8f0
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2024-01-08 10:05:27 +0100

    update version to 0.2.0

--- smartscan/__init__.py
+++ smartscan/__init__.py
@@ -1,9 +1,6 @@
 
-from .controller import SGM4Controller
 from .virtualSGM4 import VirtualSGM4
-from .file import SGM4FileManager
-from .scanner import SmartScan
-from .gpscanner import SmartScanGP
+from .asyncscanner import AsyncScanManager
 
-__version__ = "0.1.0"
-__all__ = [SmartScan, SmartScanGP, SGM4Controller, SGM4FileManager, VirtualSGM4]
+__version__ = "0.2.0"
+__all__ = [AsyncScanManager, VirtualSGM4]

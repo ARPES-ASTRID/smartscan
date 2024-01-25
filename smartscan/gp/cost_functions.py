ef4285340e27876c26d4b927ffbc44a6c1eeb4f3
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2024-01-25 09:25:36 +0100

    remove duplicate line

--- smartscan/gp/cost_functions.py
+++ smartscan/gp/cost_functions.py
@@ -181,7 +181,6 @@ def manhattan_avoid_repetition(
         x_ = None
         for xx in x:
             all_distances = np.linalg.norm(xx-prev_points, axis=1)
-            all_distances = np.linalg.norm(xx-prev_points)
             cur_min = np.min(all_distances)
             if cur_min < min_dist:
                 x_ = xx

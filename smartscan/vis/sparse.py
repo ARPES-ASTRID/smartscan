5aec4a11a4c9185c1db5d111cd2fe3548416b754
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2024-04-08 11:39:56 +0200

    formatting updates

--- smartscan/vis/sparse.py
+++ smartscan/vis/sparse.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from itertools import product
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -8,35 +9,39 @@ from scipy.spatial import Voronoi
 from typing import Literal, Sequence
 from scipy.interpolate import griddata
 import xarray as xr
+
 __all__ = [
-    'min_step_size',
-    'interpolate_sparse_array',
-    'voronoi_polygon_plot',
+    "min_step_size",
+    "interpolate_points_to_array",
+    "voronoi_polygon_plot",
 ]
 
+
 def min_step_size(arr):
-    """ Returns the minimum step size in a 1D array """
+    """Returns the minimum step size in a 1D array"""
     b = np.diff(np.sort(arr))
-    return b[b>0].min()
+    return b[b > 0].min()
+
 
 def interpolate_points_to_array(
-        positions: np.ndarray, 
-        values: np.ndarray, 
-        method: Literal['nearest','linear','cubic'] = 'nearest',
-        dims: Sequence[str] = ['x','y'],
-        coords: dict[np.ndarray | Sequence[float]] = None,
-        max_size: int = 1000,
-        attrs: dict = None,
-        name: str = None,
-        border_steps:int = 20,
-        **kwargs
-    ) -> xr.DataArray:
-    """ Interpolate a set of points to a regular grid.
+    positions: np.ndarray,
+    values: np.ndarray,
+    method: Literal["nearest", "linear", "cubic"] = "nearest",
+    dims: Sequence[str] = ["x", "y"],
+    coords: dict[np.ndarray | Sequence[float]] = None,
+    max_size: int = 1000,
+    attrs: dict = None,
+    name: str = None,
+    border_steps: int = 20,
+    **kwargs,
+) -> xr.DataArray:
+    """Interpolate a set of points to a regular grid.
 
     Args:
         positions: The positions of the points to interpolate.
         values: The values of the points to interpolate.
         method: The interpolation method to use. One of 'nearest', 'linear', or 'cubic'.
+            Nearest produces a pixely version of a vornoi plot.
         dims: The names of the dimensions of the output array.
         coords: The coordinates of the output array. If None, the coordinates are inferred from the positions.
         max_size: The maximum size of the output array. If the inferred size is larger than this, the array is truncated.
@@ -44,53 +49,66 @@ def interpolate_points_to_array(
         name: The name of the output array.
         border_steps: The number of steps to add to the border of the output array.
         **kwargs: Additional arguments to pass to scipy.interpolate.griddata.
-    
+
     Returns:
         The interpolated array.
     """
     if coords is None:
-        xmax, xmin, xstep = positions[:,0].max(), positions[:,0].min(), min_step_size(positions[:,0])
-        xmin, xmax = xmin - border_steps*xstep, xmax + border_steps*xstep
-        ymax ,ymin, ystep = positions[:,1].max(), positions[:,1].min(), min_step_size(positions[:,1])
-        ymin, ymax = ymin - border_steps*ystep, ymax + border_steps*ystep
-        xlen = min(int((xmax-xmin)/xstep), max_size)
-        ylen = min(int((ymax-ymin)/ystep), max_size)
+        xmax, xmin, xstep = (
+            positions[:, 0].max(),
+            positions[:, 0].min(),
+            min_step_size(positions[:, 0]),
+        )
+        xmin, xmax = xmin - border_steps * xstep, xmax + border_steps * xstep
+        ymax, ymin, ystep = (
+            positions[:, 1].max(),
+            positions[:, 1].min(),
+            min_step_size(positions[:, 1]),
+        )
+        ymin, ymax = ymin - border_steps * ystep, ymax + border_steps * ystep
+        xlen = min(int((xmax - xmin) / xstep), max_size)
+        ylen = min(int((ymax - ymin) / ystep), max_size)
         coords = {
-            dims[0]:np.linspace(xmin,xmax,xlen), 
-            dims[1]:np.linspace(ymin,ymax,ylen), 
+            dims[0]: np.linspace(xmin, xmax, xlen),
+            dims[1]: np.linspace(ymin, ymax, ylen),
         }
-    if attrs is None: 
+    else:
+        xlen, ylen = len(coords[dims[0]]), len(coords[dims[1]])
+    if attrs is None:
         attrs = {}
-    attrs.update({
-        'interp_method':method, 
-        'truncated':any([xlen==max_size, ylen==max_size])
-    })
+    attrs.update(
+        {
+            "interp_method": method,
+            "truncated": any([xlen == max_size, ylen == max_size]),
+        }
+    )
     grid_x, grid_y = np.meshgrid(coords[dims[1]], coords[dims[0]])
     sparse_data = griddata(positions, values, (grid_x, grid_y), method=method, **kwargs)
     xarr = xr.DataArray(
-        data = sparse_data,
-        coords = coords,
-        dims = dims,
-        attrs = attrs,
-        name = name,
+        data=sparse_data,
+        coords=coords,
+        dims=dims,
+        attrs=attrs,
+        name=name,
     )
     return xarr
 
+
 def voronoi_polygon_plot(
-        points: np.ndarray, 
-        values: np.ndarray = None, 
-        cmap: Callable | str = cm.viridis, 
-        draw_points: bool = True,
-        draw_lines: bool = True,
-        point_color: np.ndarray | str = None,
-        line_color: np.ndarray | str = None,
-        alpha: float = 0.4,
-        ax: plt.Axes = None, 
-        border: float | None = 0.02,
-    ) -> None:
+    points: np.ndarray,
+    values: np.ndarray,
+    cmap: Callable | str = "viridis",
+    draw_points: bool = True,
+    draw_lines: bool = True,
+    point_color: np.ndarray | str | None = None,
+    line_color: np.ndarray | str | None = None,
+    alpha: float = 0.4,
+    ax: plt.Axes | None = None,
+    border: float | None = 0.02,
+) -> plt.Axes:
     """
     Plot the Voronoi diagram of a set of points.
-    
+
     Args:
         points: The points to plot.
         values: The values to use for coloring the Voronoi regions.
@@ -103,20 +121,32 @@ def voronoi_polygon_plot(
         ax: The axis to plot on.
         border: The border to add to the x and y limits. If None, no restriction on the
             limits is applied.
-        
+
     Returns:
         None
     """
 
     if ax is None:
-        ax = plt.gca()
+        ax: plt.Axes = plt.gca()
 
     # add points outside the box to draw border polygons
-    limits = np.asarray([np.min(points[:,0]), np.max(points[:,0]), np.min(points[:,1]), np.max(points[:,1])])
+    limits = np.asarray(
+        [
+            np.min(points[:, 0]),
+            np.max(points[:, 0]),
+            np.min(points[:, 1]),
+            np.max(points[:, 1]),
+        ]
+    )
     box_size = limits[1] - limits[0], limits[3] - limits[2]
-    double_limits = limits[0] - box_size[0], limits[1] + box_size[0], limits[2] - box_size[1], limits[3] + box_size[1]
+    double_limits = (
+        limits[0] - box_size[0],
+        limits[1] + box_size[0],
+        limits[2] - box_size[1],
+        limits[3] + box_size[1],
+    )
     double_corners = np.asarray(list(product(double_limits[:2], double_limits[2:])))
-    points = np.append(points, double_corners, axis = 0)
+    points = np.append(points, double_corners, axis=0)
 
     # compute Voronoi tesselation
     vor = Voronoi(points)
@@ -128,30 +158,34 @@ def voronoi_polygon_plot(
 
     for point_id, region_id in enumerate(vor.point_region):
         region = vor.regions[region_id]
-        if not -1 in region:
+        if -1 not in region:  # finite region
             polygon = [vor.vertices[i] for i in region]
             if draw_lines:
                 edgecolor = colors[point_id] if line_color is None else line_color
             else:
                 edgecolor = None
-            ax.fill(*zip(*polygon), alpha=alpha, color=colors[point_id], edgecolor=edgecolor)
+            ax.fill(
+                *zip(*polygon), alpha=alpha, color=colors[point_id], edgecolor=edgecolor
+            )
 
     if draw_points:
         point_color = colors if point_color is None else point_color
-        ax.scatter(points[:-4,0], points[:-4,1], color=point_color, marker='o', s=2)
-
+        scatter = ax.scatter(points[:-4, 0], points[:-4, 1], color=point_color, marker="o", s=2)
+    else:
+        scatter = None
     if border is not None:
-        ax.set_xlim(limits[0] - box_size[0] * border, limits[1] +  box_size[0] * border)
-        ax.set_ylim(limits[2] - box_size[1] * border, limits[3] +  box_size[1] * border)
-    return ax
+        ax.set_xlim(limits[0] - box_size[0] * border, limits[1] + box_size[0] * border)
+        ax.set_ylim(limits[2] - box_size[1] * border, limits[3] + box_size[1] * border)
+    return ax, scatter
+
 
 ##############
 # DEPRECATED #
 ##############
 def voronoi_finite_polygons_2d(
-        vor: Voronoi,
-        radius: float = None,
-    ) -> tuple:
+    vor: Voronoi,
+    radius: float = None,
+) -> tuple:
     """
     Reconstruct infinite voronoi regions in a 2D diagram to finite
     regions.
@@ -214,7 +248,7 @@ def voronoi_finite_polygons_2d(
 
             # Compute the missing endpoint of an infinite ridge
 
-            t = vor.points[p2] - vor.points[p1] # tangent
+            t = vor.points[p2] - vor.points[p1]  # tangent
             t /= np.linalg.norm(t)
             n = np.array([-t[1], t[0]])  # normal
 
@@ -228,7 +262,7 @@ def voronoi_finite_polygons_2d(
         # sort region counterclockwise
         vs = np.asarray([new_vertices[v] for v in new_region])
         c = vs.mean(axis=0)
-        angles = np.arctan2(vs[:,1] - c[1], vs[:,0] - c[0])
+        angles = np.arctan2(vs[:, 1] - c[1], vs[:, 0] - c[0])
         new_region = np.array(new_region)[np.argsort(angles)]
 
         # finish
@@ -238,16 +272,16 @@ def voronoi_finite_polygons_2d(
 
 
 def plot_vornoi_diagram(
-        points: np.ndarray, 
-        values: np.ndarray = None, 
-        cmap: Callable | str = cm.viridis, 
-        draw_points: bool = True,
-        draw_lines: bool = True,
-        point_color: np.ndarray | str = None,
-        line_color: np.ndarray | str = None,
-        ax: plt.Axes = None, 
-        border: float | None = 0.02,
-    ) -> None:
+    points: np.ndarray,
+    values: np.ndarray = None,
+    cmap: Callable | str = cm.viridis,
+    draw_points: bool = True,
+    draw_lines: bool = True,
+    point_color: np.ndarray | str = None,
+    line_color: np.ndarray | str = None,
+    ax: plt.Axes = None,
+    border: float | None = 0.02,
+) -> None:
     """
     Plot the Voronoi diagram of a set of points.
 
@@ -260,7 +294,7 @@ def plot_vornoi_diagram(
         point_color: The color to use for the points.
         line_color: The color to use for the lines.
         ax: The axis to plot on.
-        border: The border to add to the x and y limits. If None, no restriction on the 
+        border: The border to add to the x and y limits. If None, no restriction on the
             limits is applied.
 
     Returns:
@@ -272,7 +306,7 @@ def plot_vornoi_diagram(
         cmap = plt.get_cmap(cmap)
     if values is None:
         values = np.linspace(0, 1, len(points))
-    #normalize values to [0,1]
+    # normalize values to [0,1]
     values = (values - np.min(values)) / (np.max(values) - np.min(values))
     colors = cmap(values)
 
@@ -280,7 +314,7 @@ def plot_vornoi_diagram(
     regions, vertices = voronoi_finite_polygons_2d(vor)
 
     # colorize
-    for region, color in zip(regions,colors):
+    for region, color in zip(regions, colors):
         polygon = vertices[region]
         edgecolor = None
         if draw_lines:
@@ -289,17 +323,17 @@ def plot_vornoi_diagram(
 
     if draw_points:
         point_color = colors if point_color is None else point_color
-        ax.scatter(points[:,0], points[:,1], color=point_color, marker='o', s=2)
+        ax.scatter(points[:, 0], points[:, 1], color=point_color, marker="o", s=2)
 
     if border is not None:
-        ptp = vor.max_bound[0] - vor.min_bound[0], vor.max_bound[1] - vor.min_bound[1] 
-        ax.set_xlim(vor.min_bound[0] - ptp[0] * border, vor.max_bound[0] + ptp[0] * border)
-        ax.set_ylim(vor.min_bound[1] - ptp[0] * border, vor.max_bound[1] + ptp[0] * border)
+        ptp = vor.max_bound[0] - vor.min_bound[0], vor.max_bound[1] - vor.min_bound[1]
+        ax.set_xlim(
+            vor.min_bound[0] - ptp[0] * border, vor.max_bound[0] + ptp[0] * border
+        )
+        ax.set_ylim(
+            vor.min_bound[1] - ptp[0] * border, vor.max_bound[1] + ptp[0] * border
+        )
 
 
-if __name__ == '__main__':
-
-    points = np.random.rand(500,2)
-    values = np.abs(points[:,0]-.5)**0.5 + np.abs(points[:,1]-.5,)**0.5 #np.random.rand(60)
-    values += np.random.normal(0, 0.05, len(values))
-    vornoi_plot(points, values, cmap='hot')
+if __name__ == "__main__":
+    pass

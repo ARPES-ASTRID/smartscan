2c8241ac6478fd11c8c63cccb9bb6a21cb4d6323
Steinn Ymir Agustsson <agustsson@phys.au.dk>
2024-04-10 11:20:06 +0200

    up version to 0.5.0

--- smartscan/__init__.py
+++ smartscan/__init__.py
@@ -1,9 +1,9 @@
-
 import os
+# Fix for Intel OpenMP runtime error on Windows
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from .asyncscanner import AsyncScanManager
 from .virtualSGM4 import VirtualSGM4
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __all__ = [AsyncScanManager, VirtualSGM4]
